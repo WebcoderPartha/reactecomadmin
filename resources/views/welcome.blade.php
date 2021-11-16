d402bfc0f8d09173878be145a198655614de050f
Parthadeb Mondal <parthadev76@gmail.com>
2021-11-16 16:25:42 +0600

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 <script src="{{ asset('backend/js/vendors.min.js') }}"></script>
 <script src="{{ asset('backend/assets/icons/feather-icons/feather.min.js') }}"></script>
 {{--<script src="{{ asset('backend/assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>--}}
-<script src="{{ asset('backend/assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
+{{--<script src="{{ asset('backend/assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>--}}
 {{--<script src="{{ asset('backend/assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>--}}
 
 <!-- Sunny Admin App -->
